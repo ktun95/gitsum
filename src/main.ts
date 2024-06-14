9673b7bebb9b8149cba57ab73308bb08beeb7c1b
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-13 21:53:10 -0500

    test

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { ChatOpenAI } from "@langchain/openai";
 
-const model = new ChatOpenAI({model: "gpt-3"});
+const model = new ChatOpenAI({model: "gpt-3.5-turbo"});
 
 const messages = [
   new SystemMessage("Translate the following from English into Italian"),
   new HumanMessage("hi!"),
 ];
 
-await model.invoke(messages);
-
+console.log(await model.invoke(messages));
