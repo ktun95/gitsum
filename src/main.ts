890c9b2ffae36ffbdee390abe067fe146061e6f8
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-14 21:34:58 -0500

    more

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,30 @@
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { ChatOpenAI } from "@langchain/openai";
+import { execSync } from "child_process"
 
-const model = new ChatOpenAI({model: "gpt-3.5-turbo"});
+const LLM_MODEL = new ChatOpenAI({ model: "gpt-3.5-turbo" });
+const SYSTEM_PROMPT = "Generate a bullet pointed summary from the provided git commit messages"
 
-const messages = [
-  new SystemMessage("Translate the following from English into Italian"),
-  new HumanMessage("hi!"),
-];
+const summarize = async () => {
+  // probably need to handle this buffer in some way. If the output of gitlog is too long, it will result in a buffer overflow error
+  const buffer = execSync("git log --since=2024-06-1");
+  const messages = [
+    new SystemMessage(SYSTEM_PROMPT),
+    new HumanMessage(buffer.toString("utf-8")),
+  ];
+  const { content } = await LLM_MODEL.invoke(messages);
+  console.log(content);
+}
 
-console.log(await model.invoke(messages));
+
+const main = () => {
+  switch(process.argv[2]) {
+    case "summarize":
+      summarize();
+      break;
+    default:
+      process.exit(1)
+  }
+}
+
+main();
